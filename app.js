a9856f1286d37dc9ebd663fbe343542ea02cd34a
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-10 12:12:12 +0100

    Make a seperate file for domContent

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 function Player(name, symbol) {
   return { name, symbol };
 }
@@ -7,47 +8,6 @@ const Board = (() => {
   const gameState = new Array(9).fill('');
 
   return { gameState };
-})();
-
-
-const domContent = (() => {
-  const currentPlayerTurn = (cp) => `It's ${cp}'s turn`;
-  const winningMessage = (currentPlayer) => `Player ${currentPlayer} has won!`;
-  const drawMessage = () => 'Game ended in a draw!';
-  const statusDisplay = document.querySelector('.game--status');
-  const board = document.querySelector('.board');
-  const form = document.getElementById('form');
-  const alert = document.querySelector('.alert');
-
-  const showBoard = () => {
-    alert.classList.add('d-none');
-    form.classList.add('d-hidden');
-    board.classList.remove('d-hidden');
-  };
-
-  const removeBoard = () => {
-    form.classList.remove('d-hidden');
-    board.classList.add('d-hidden');
-  };
-
-  const showAlert = () => alert.classList.remove('d-none');
-
-  const clearFields = () => {
-    document.querySelector('#fplayer').value = '';
-    document.querySelector('#splayer').value = '';
-  };
-
-
-  return {
-    currentPlayerTurn,
-    winningMessage,
-    drawMessage,
-    showBoard,
-    statusDisplay,
-    showAlert,
-    removeBoard,
-    clearFields,
-  };
 })();
 
 
---
+++ domContent.js
@@ -0,0 +1,39 @@
+const domContent = (() => { // eslint-disable-line
+  const currentPlayerTurn = (cp) => `It's ${cp}'s turn`;
+  const winningMessage = (currentPlayer) => `Player ${currentPlayer} has won!`;
+  const drawMessage = () => 'Game ended in a draw!';
+  const statusDisplay = document.querySelector('.game--status');
+  const board = document.querySelector('.board');
+  const form = document.getElementById('form');
+  const alert = document.querySelector('.alert');
+
+  const showBoard = () => {
+    alert.classList.add('d-none');
+    form.classList.add('d-hidden');
+    board.classList.remove('d-hidden');
+  };
+
+  const removeBoard = () => {
+    form.classList.remove('d-hidden');
+    board.classList.add('d-hidden');
+  };
+
+  const showAlert = () => alert.classList.remove('d-none');
+
+  const clearFields = () => {
+    document.querySelector('#fplayer').value = '';
+    document.querySelector('#splayer').value = '';
+  };
+
+
+  return {
+    currentPlayerTurn,
+    winningMessage,
+    drawMessage,
+    showBoard,
+    statusDisplay,
+    showAlert,
+    removeBoard,
+    clearFields,
+  };
+})();
